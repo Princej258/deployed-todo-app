fa3ce58d524f9ce8d53e5ee8370052988d213e83
Prince Jassal <74393262+Princej258@users.noreply.github.com>
2023-12-28 18:14:23 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,6 +7,11 @@ const pool = require('./db')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
+app.listen(port, () => {
+  console.log('Hello world listening on port', port);
+});
+
+
 app.use(cors())
 app.use(express.json())
 
